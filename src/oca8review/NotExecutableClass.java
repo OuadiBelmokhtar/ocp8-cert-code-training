97c04a26aaaa8f3b493f2da9e84ee4f55edf4acb
Ouadi Belmokhtar <ouadi.belmokhtar@gmail.com>
2019-09-04 17:35:12 +0100

    first commit

--- src/oca8review/NotExecutableClass.java
+++ src/oca8review/NotExecutableClass.java
@@ -1,7 +1,14 @@
 package oca8review;
 
 public class NotExecutableClass {
-	public static void mai(String[] args) {
-		System.out.println("ok");
+	public static void main(String[] args) {
+	System.out.println(getInt());
+		
+			
+	}
+
+	public static Integer getInt() {
+		int i = 10;
+		return i; // Autoboxing i
 	}
 }
