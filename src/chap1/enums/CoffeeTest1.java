2afdcc1f0d773b4a34611f797ba082cdb5e9d835
Ouadi Belmokhtar <ouadi.belmokhtar@gmail.com>
2019-09-20 17:20:54 +0100

    typo

--- src/chap1/enums/CoffeeTest1.java
+++ src/chap1/enums/CoffeeTest1.java
@@ -3,14 +3,20 @@ package chap1.enums;
 import chap1.enums.Coffee.CoffeeName;
 
 public class CoffeeTest1 {
+	static CoffeeSize enumVar;
 
 	public static void main(String[] args) {
+		// access using var but not the class name
+		System.out.println(enumVar.BIG);
+		System.out.println(CoffeeSize.BIG);
+
 		// If the enum is imported as above, using class name as prefix becomes optional
 		CoffeeName name = CoffeeName.CAPPUCINO;
-		String strName = Coffee.CoffeeName.CAPPUCINO.toString();
+		String strName = Coffee.CoffeeName.CAPPUCINO.toString(); 
 		// ...
 
 		CoffeeSize enumSize = CoffeeSize.HUGE;
+
 		Object objSize = CoffeeSize.HUGE;
 		String strSize = CoffeeSize.HUGE.name();
 		Coffee coffee1 = new Coffee(name, enumSize);
@@ -33,12 +39,12 @@ public class CoffeeTest1 {
 			System.out.println(cs); // prints all enums
 		//
 		System.out.println("*Affichage des Mls des enums");
-		for (CoffeeSize cs: CoffeeSize.values())
-			System.out.println(cs +" "+ cs.getMls());
+		for (CoffeeSize cs : CoffeeSize.values())
+			System.out.println(cs + " " + cs.getMls());
 		//
 		System.out.println("*Affichage via indice");
 		System.out.println(CoffeeSize.values()[0]); // print BIG
-		
+
 		// System.out.println(CoffeeSize.HUGE + " " + hugeMl + " mls");
 	}
 }
