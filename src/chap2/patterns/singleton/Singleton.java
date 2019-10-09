b9c146e997fc5e6654ca611e94a9e5d4829160b2
Ouadi Belmokhtar <ouadi.belmokhtar@gmail.com>
2019-10-09 17:04:37 +0100

    typo

--- src/chap2/patterns/singleton/Singleton.java
+++ src/chap2/patterns/singleton/Singleton.java
@@ -1,23 +1,26 @@
 package chap2.patterns.singleton;
 
-// Impl using Lazy init and creation code look
+// Impl using Lazy init and creation code look, with double-checked locking practice
 public class Singleton {
 
-	private static Singleton INSTANCE = null;
+	private static Singleton instance = null;
 
 	private Singleton() {
 		System.out.println("Instanciation via constructeur");
 	}
 
 	public static Singleton getInstance() { // Don’t synchronize complete method
-		synchronized (Singleton.class) { // Synchronizing just code block that creates new instance.
-			if (INSTANCE == null) {
-				INSTANCE = new Singleton(); // Lazy initialization
+		if (instance == null) { // double-checked locking, in order to not perform block locking for each call
+								// to this method. Which enhances exec performance.
+			synchronized (Singleton.class) { // Synchronizing just code block that creates new instance.
+				if (instance == null) {
+					instance = new Singleton(); // Lazy initialization
+				}
 			}
 		}
-
-		return INSTANCE;
+		return instance;
 	}
 
 }
+
 
