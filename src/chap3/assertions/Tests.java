124bb43d6955987cfdcea7a1ca0b678d65c7d7a0
Ouadi Belmokhtar <ouadi.belmokhtar@gmail.com>
2019-10-22 17:28:01 +0100

    typo

---
+++ src/chap3/assertions/Tests.java
@@ -0,0 +1,10 @@
+package chap3.assertions;
+
+public class Tests {
+	public static void main(String[] args) {
+		int a = 10;
+		assert a >10:"sup a 10";
+		System.out.println("ok");
+		
+	}
+}
---
+++ src/chap3/exceptions/Tests.java
@@ -0,0 +1,14 @@
+package chap3.exceptions;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class Tests {
+	public static void main(String[] args) {
+Closeable
+		
+		
+		
+
+	}
+}
