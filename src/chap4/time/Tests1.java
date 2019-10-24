e2c158cb9ee7c65aadef3db7082c9cf5d1e5fad5
Ouadi Belmokhtar <ouadi.belmokhtar@gmail.com>
2019-10-24 12:29:05 +0100

    typo

--- src/chap4/time/Tests1.java
+++ src/chap4/time/Tests1.java
@@ -1,5 +1,6 @@
 package chap4.time;
 
+import java.time.Duration;
 import java.time.Instant;
 
 public class Tests1 {
@@ -7,5 +8,9 @@ public static void main(String[] args) {
 	
 	System.out.println(Instant.now());
 	// 2019-10-23T11:24:07.475Z
+	
+	System.out.println(Duration.ofDays(2)); // PT48H
+	System.out.println(Duration.ofMinutes(30)); // PT30M
+	System.out.println(Duration.ofMinutes(150)); // PT2H30M
 }
 }
