13ad00e0e3a5d69dbcaadd354738a988acd07061
ouadi.belmokhtar <ouadi.belmokhtar@gmail.com>
2019-11-14 18:21:25 +0100

    typo

--- src/chap5/nio2/Tests1.java
+++ src/chap5/nio2/Tests1.java
@@ -9,26 +9,29 @@ import java.util.List;
 
 public class Tests1 {
 	public static void main(String[] args) {
-//		Path path = Paths.get("fich2.txt");
-//		try {
-//			// Files.createFile(path);
-//			// List<String> lines=Files.readAllLines(path);
-//			List<String> lines = new ArrayList<String>();
-//			lines.add("ahmed");
-//			lines.add("Taha");
-//			Files.readAllLines(path).get(1);
-//			Files.write(path, lines);
-//			for (String l : lines)
-//				System.out.println(l);
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+
+		try {
+			Path path = Paths.get("fich3.txt");
+			Files.createFile(path);
+			List<String> lines1 = new ArrayList<String>();
+			lines1.add("ahmed");
+			lines1.add("Taha");
+			lines1.add("Karim");
+			// Ecrire ds le fichier
+			Files.write(path, lines1);
+			// Lire le contenu du fichier
+			List<String> lines2 = Files.readAllLines(path);
+
+			for (String l : lines2)
+				System.out.println(l);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 		try {
 			Path path2 = Paths.get("a/b/c");
 			Files.createDirectories(path2);
-			// Path path3=Paths.get("a/b/c/f1.txt"); 
+			// Path path3=Paths.get("a/b/c/f1.txt");
 			Path path3 = Paths.get(path2.toString(), "f1.txt");
 			if (Files.notExists(path3)) {
 				Files.createFile(path3);
