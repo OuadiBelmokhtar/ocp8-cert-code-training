b344024458027ba215b0d9bbc8e1577373f04aaa
ouadi.belmokhtar <ouadi.belmokhtar@gmail.com>
2020-04-11 23:51:19 +0100

    add chap9

--- src/chap6/generics/Tests3.java
+++ src/chap6/generics/Tests3.java
@@ -1,13 +1,11 @@
 package chap6.generics;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.PriorityQueue;
+import java.util.TreeSet;
 
-public class Tests3 {
+public class Tests3<T extends String> {
 	public static void main(String[] args) {
 		Map<Animal, String> map = new HashMap<Animal, String>();
 		Animal dog = new Animal();
@@ -18,6 +16,13 @@ public class Tests3 {
 		System.out.println(map.get(dog));// null, because hashCode() fails
 		dog.nom = "bobino";
 		System.out.println(map.get(dog)); // Dog animal
-		
+		PriorityQueue<String> pq = new PriorityQueue<String>();
+
+		TreeSet ts = new TreeSet();
+		ts.add(new Object());// throws ClassCastException: Object class cannot be cast to Comparable
+		int a, b, c;
+		for (a = 1, b = 3, c = a * b; a < 10; a++) {
+
+		}
 	}
 }
