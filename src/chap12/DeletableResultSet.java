0b478e8f2dc49ba45b0a627f61453bde97cf575a
ouadi.belmokhtar <ouadi.belmokhtar@gmail.com>
2020-12-14 10:39:51 +0100

    add DeletableResulSet examples to chap12

--- src/chap12/DeletableResultSet.java
+++ src/chap12/DeletableResultSet.java
@@ -2,15 +2,14 @@ package chap12;
 
 import static java.lang.System.out;
 
-import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class UpdatableResulSet {
+public class DeletableResultSet {
+
 	public static void main(String[] args) {
 		String URL = "jdbc:postgresql://localhost:5432/booksellerdb"; // port is optional
 		String userName = "postgres";
@@ -21,20 +20,16 @@ public class UpdatableResulSet {
 			// using an UPDATABLE RS
 			Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
 			ResultSet rs = stmt.executeQuery("SELECT ISBN, title, unitprice FROM book");
+			
 			out.println("## All books");
 			while (rs.next()) {
 				out.println(rs.getString("title") + " " + rs.getFloat("unitprice"));
 			}
-			// reinitialiser la position du curseur
-			rs.beforeFirst();
-			while (rs.next()) {
-				
-				if (rs.getDouble("unitprice")  == 8.95) {
-					rs.updateDouble("unitprice", 12.95);
-					rs.updateRow();
-					
-				}
-			}
+			// positionner le curseur surla ligne 2
+			rs.absolute(2);
+			// supprimer la ligne courante
+			rs.deleteRow();
+			
 			// reinitialiser la position du curseur
 			rs.beforeFirst();
 			out.println("\n## All books after update");
