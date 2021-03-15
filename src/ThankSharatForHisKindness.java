588bfa0caa8704f751b28746ddf34e7083f7ce46
ouadi.belmokhtar <ouadi.belmokhtar@gmail.com>
2021-03-15 15:34:28 +0100

    typo

--- src/ThankSharatForHisKindness.java
+++ src/ThankSharatForHisKindness.java
@@ -4,10 +4,10 @@ public class ThankSharatForHisKindness {
 	public static void main(String[] args) {
 		
 		StringBuffer myLaptopMsg = new StringBuffer();
-		myLaptopMsg.append("Thank you for your kindness Charat. \n");
+		myLaptopMsg.append("Thank you for your kindness Sharat. \n");
 		myLaptopMsg.append("Can't wait for the upgrade! \n");
 		myLaptopMsg.append("If you have something to wear I'll be grateful to you. \n");
-		myLaptopMsg.append("Have a nice week start.");
+		myLaptopMsg.append("have a good week.");
 		System.console().writer().println(myLaptopMsg);
 			
 	}
